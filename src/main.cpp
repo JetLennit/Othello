12c4c68a14d82313f78f1bdc0c4768a1d651433e
jetlennit <jetlennit@gmail.com>
2021-12-23 13:42:29 -0500

    Added command line options and readded terminal output

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <string.h>
 #include <SDL.h>
 #include <game.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 660;
 
-int main() {
+int textual() {
+    std::cout << std::endl;
+    Game game;
+    while (true) {
+        game.draw();
+        if(!game.checkInput())
+            break;
+        game.update();
+        break;
+    }
+    game.finish();
+    game.draw();
+    std::cout << "Game exited" << std::endl;
+
+    return 0;
+}
+
+int graphical() {
     Game game;
 
     SDL_Window* window = NULL;
@@ -50,6 +68,56 @@ int main() {
     SDL_DestroyWindow( window );
 
     SDL_Quit();
+
+    return 0;
+}
+
+void help() {
+    std::cout << "Usage:                          " << std::endl 
+              << "  ./othello [options]           " << std::endl << std::endl 
+              << "Options:                        " << std::endl
+              << "  -t       Text mode            " << std::endl
+              << "  -c <ip>  Connect to host at ip" << std::endl
+              << "  -s       Host server          " << std::endl
+              << "  -h, -?   Print this screen    " << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) 
+        return graphical();
+
+    bool graphics = true;
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-' && strlen(argv[i]) != 1) {
+            switch (argv[i][1]) {
+                case 't':
+                    graphics = false;
+                    break;
+                case 'c':
+                    // for network code later
+                    if (i + 1 < argc) 
+                        std::cout << "Connecting to " << argv[i + 1] << std::endl;
+                    else
+                        std::cout << "Please add IP after -c" << std::endl;
+                    break;
+                case 's':
+                    //for network code later
+                    std::cout << "Hosting" << std::endl;
+                    break;
+                case 'h':
+                case '?':
+                    help();
+                    return 0;
+                    break;
+            }
+        }
+    }
+
+    if (graphics)
+        return graphical();
+    else 
+        return textual();
 
     return 0;
 }
